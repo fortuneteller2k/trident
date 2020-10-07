571eb5714566f505b9b2c6e1d7e63924e7f18cc9
fortuneteller2k <lythe1107@gmail.com>
2020-10-07 12:47:48 +0800

    add opus type

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -67,7 +67,7 @@ export default class PlayCommand extends Command {
             return;
         }
 
-        const dispatcher = guildQueue.connection.play(await ytdl(track.url))
+        const dispatcher = guildQueue.connection.play(await ytdl(track.url), { type: "opus" })
                                                 .on("finish", () => {
                                                     guildQueue.tracks.shift();
                                                     this.play(guild, guildQueue.tracks[0]);
