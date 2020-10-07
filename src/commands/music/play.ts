a06889434b34952a813ef3de4bacf834532ef5b4
fortuneteller2k <lythe1107@gmail.com>
2020-10-07 18:13:19 +0800

    finally a working implementation of search

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -1,9 +1,11 @@
 import { Command } from "discord-akairo";
-import { Guild } from "discord.js";
-import { StreamDispatcher } from "discord.js";
-import { Message, VoiceConnection, TextChannel, VoiceChannel } from "discord.js";
-import * as yt from "youtube-search-without-api-key";
-import ytdl from "ytdl-core-discord";
+import { Guild, Message, StreamDispatcher, TextChannel, VoiceChannel, VoiceConnection } from "discord.js";
+import ytdld from "ytdl-core-discord";
+import ytdlc from "ytdl-core";
+import youtubeSearch from "youtube-search";
+import { config } from "dotenv";
+
+config({ "path": "../../../../.env" });
 
 export const queue: Map<string, QueueContract> = new Map();
 
@@ -46,7 +48,7 @@ class QueueContract {
 export default class PlayCommand extends Command {
     public constructor() {
         super("play", { 
-            aliases: ["p", "play"],
+            aliases: ["p"],
             category: "music",
             args: [{
                 id: "query",
@@ -70,7 +72,7 @@ export default class PlayCommand extends Command {
             return;
         }
 
-        guildQueue.dispatcher = guildQueue.connection.play(await ytdl(track.url), { type: "opus" })
+        guildQueue.dispatcher = guildQueue.connection.play(await ytdld(track.url), { type: "opus" })
                                                      .on("finish", () => {
                                                          guildQueue.tracks.shift();
                                                          this.play(guild, guildQueue.tracks[0]);
@@ -79,47 +81,56 @@ export default class PlayCommand extends Command {
 
         
         guildQueue.dispatcher.setVolumeLogarithmic(guildQueue.volume / 5);
-        guildQueue.textChannel.send(`**Now playing:** ${track.title}`);
+        await guildQueue.textChannel.send(`**Now playing:** ${track.title}`);
     }
 
     isURL = (str: string) => {
-        var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
-        var url = new RegExp(urlRegex, 'i');
+        const urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+        const url = new RegExp(urlRegex, 'i');
         return str.length < 2083 && url.test(str);
     }
 
-    postVerify = async (msg: Message, query: string) => {
+    processTracks = async (msg: Message, query: string) => {
+        console.log(query);
+
         const vc = msg.member.voice.channel;
         const guildQueue = queue.get(msg.guild.id);
 
-        const trackInfo = await ytdl.getInfo(query);
-        const track = new Track(trackInfo.videoDetails.title, trackInfo.videoDetails.video_url);
+        await ytdlc.getInfo(query).then(async trackInfo => {
+            const track = new Track(trackInfo.videoDetails.title, trackInfo.videoDetails.video_url);
 
-        if (!guildQueue) {
-            const queueContract: QueueContract = new QueueContract(<TextChannel>msg.channel, vc, null, null, [], 5, true);
-            queue.set(msg.guild.id, queueContract);
-
-            queueContract.tracks.push(track);
-
-            try {
-               const conn = await vc.join();
-               queueContract.connection = conn;
-                this.play(msg.guild, queueContract.tracks[0]);
-            } catch (e) {
-               queue.delete(msg.guild.id);
-              return msg.channel.send(e);
+            if (!guildQueue) {
+                const queueContract: QueueContract = new QueueContract(<TextChannel>msg.channel, vc, null, null, [], 10, true);
+                queue.set(msg.guild.id, queueContract);
+    
+                queueContract.tracks.push(track);
+    
+                try {
+                    queueContract.connection = await vc.join();
+                    await this.play(msg.guild, queueContract.tracks[0]);
+                } catch (e) {
+                   queue.delete(msg.guild.id);
+                  return msg.channel.send(e);
+                }
+            } else {
+                guildQueue.tracks.push(track);
+                return msg.reply(`**${track.title}** added to the queue.`);
             }
-        } else {
-            guildQueue.tracks.push(track);
-            return msg.reply(`**${track.title}** added to the queue.`);
-        }
+        });
     }
 
-    public exec = (msg: Message, { query }: { query: string }) => {
-        if (this.isURL(query)) return this.postVerify(msg, query);
+    public exec = async (msg: Message, { query }: { query: string }) => {
+        if (this.isURL(query)) return this.processTracks(msg, query);
         else {
-            const videos = yt.search(query);
-            return this.postVerify(msg, `https://www.youtube.com/watch?v=${videos[0].id.videoId}`);
+            const options: youtubeSearch.YouTubeSearchOptions = {
+                maxResults: 1,
+                key: process.env.API_KEY
+            };
+
+            await youtubeSearch(query, options, (err, res) => {
+                if (err) console.log(err);
+                else return this.processTracks(msg, `https://www.youtube.com/watch?v=${res[0].id}`);
+            });
         }
     }
 }
